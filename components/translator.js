7eb6adf8447a106e3b8eecd7e7916f8bb50d837f
Replit user <>
2024-01-27 13:56:24 +0300

    entire project

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,49 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+  americanToBritishTranslate(string) {
+    const replaceWithHighlight = (key, value) => {
+      const regex = new RegExp(`\\b${key}(?=[\\.\\s\\,\\!\\?])`, "gi");
+      const timeRegex = new RegExp(/\b(\d{1,2}):(\d{2})\b/g);
+      return string
+        .replace(regex, `<span class="highlight">${value}</span>`)
+        .replace(timeRegex, `<span class="highlight">${"$1"}.${"$2"}</span>`);
+    };
 
+    for (let key in americanOnly) {
+      string = replaceWithHighlight(key, americanOnly[key]);
+    }
+    for (let key in americanToBritishSpelling) {
+      string = replaceWithHighlight(key, americanToBritishSpelling[key]);
+    }
+    for (let key in americanToBritishTitles) {
+      string = replaceWithHighlight(key, americanToBritishTitles[key]);
+    }
+    return string;
+  }
+
+  britishToAmericanTranslate(string) {
+    const replaceWithHighlight = (key, value) => {
+      const regex = new RegExp(`\\b${key}\\b`, "gi");
+      const timeRegex = new RegExp(/\b(\d{1,2})\.(\d{2})\b/g);
+      return string
+        .replace(regex, `<span class="highlight">${value}</span>`)
+        .replace(timeRegex, `<span class="highlight">${"$1"}:${"$2"}</span>`);
+    };
+    for (let key in britishOnly) {
+      string = replaceWithHighlight(key, britishOnly[key]);
+    }
+    for (let key in americanToBritishSpelling) {
+      string = replaceWithHighlight(americanToBritishSpelling[key], key);
+    }
+    for (let key in americanToBritishTitles) {
+      string = replaceWithHighlight(americanToBritishTitles[key], key);
+    }
+    return string;
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,31 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      if (req.body.text === '') {
+        res.json({ error: 'No text to translate' });
+        return
+      }
+      if (!req.body.text || !req.body.locale) {
+        res.json({ error: 'Required field(s) missing' });
+        return
+      }
+      if (req.body.locale !== 'american-to-british' & req.body.locale !== 'british-to-american') {
+        res.json({ error: 'Invalid value for locale field' });
+        return
+      }
+      if (req.body.locale === 'american-to-british') {
+        res.body = {}
+        res.body.text = req.body.text
+        res.body.translation = translator.americanToBritishTranslate(req.body.text)
+      }
+      if (req.body.locale === 'british-to-american') {
+        res.body = {}
+        res.body.text = req.body.text
+        res.body.translation = translator.britishToAmericanTranslate(req.body.text)
+      }
+      if (req.body.text === res.body.translation) {
+        res.body.translation = 'Everything looks good to me!'
+      }
+        res.json(res.body)
     });
 };
